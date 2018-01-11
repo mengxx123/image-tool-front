148d3ba0b3c9a4d437a8cd4f6fbed5820962b948
cjh <1418503647@qq.com>
2018-01-11 22:47:26 +0800

    update logo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
     <ui-page name="about" title="关于">
         <div class="about-box">
-            <img class="logo" src="/static/img/logo.png">
+            <img class="logo" src="/static/img/logo.svg">
             <div class="version">图片处理工具 v1.1.0</div>
             <div class="copyright">© 2018 <a href="https://yunser.com">云设</a></div>
         </div>
@@ -23,6 +23,7 @@
     }
     .logo {
         display: block;
+        width: 64px;
         margin: 80px auto 16px auto;
     }
     .version {
